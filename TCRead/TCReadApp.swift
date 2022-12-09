58f0dc650660e7b11c4fa0facfa0fd2127fe19b0
ToddCooke <toddcookevt@gmail.com>
2022-12-09 17:12:41 -0500

    Initial Commit

---
+++ TCRead/TCReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCReadApp.swift
+//  TCRead
+//
+//  Created by Todd Cooke on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TCReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
