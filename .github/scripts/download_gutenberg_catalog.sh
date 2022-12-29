63ed8a55d2c5627a6e7004d2512364dd7e67b7c5
Todd Cooke <toddcookevt@gmail.com>
2022-12-29 15:54:21 -0500

    Update download_gutenberg_catalog.sh

--- .github/scripts/download_gutenberg_catalog.sh
+++ .github/scripts/download_gutenberg_catalog.sh
@@ -4,8 +4,8 @@ echo Downloading catalog CSV
 curl https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv --output pg_catalog.csv
 
 echo Creating sqlite db
-sqlite3 books.db ".mode csv" ".import pg_catalog.csv books" ".exit"
+sqlite3 book.sqlite3 ".mode csv" ".import pg_catalog.csv books" ".exit"
 
 echo verifying that db was created successfully
-sqlite3 books.db "select * from books limit 10;" ".exit"
+sqlite3 book.sqlite3 "select * from books limit 10;" ".exit"
 [ $? -eq 0 ] && echo "success" || exit 1 
