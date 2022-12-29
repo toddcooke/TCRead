dd206378b635effc89619aab45a1e613430fa02e
Todd Cooke <toddcookevt@gmail.com>
2022-12-29 15:50:36 -0500

    verify sqlite db created successfully

--- .github/scripts/download_gutenberg_catalog.sh
+++ .github/scripts/download_gutenberg_catalog.sh
@@ -5,3 +5,7 @@ curl https://www.gutenberg.org/cache/epub/feeds/pg_catalog.csv --output pg_catal
 
 echo Creating sqlite db
 sqlite3 books.db ".mode csv" ".import pg_catalog.csv books" ".exit"
+
+echo verifying that db was created successfully
+sqlite3 books.db "select * from books limit 10;" ".exit"
+[ $? -eq 0 ] && echo "success" || exit 1 
